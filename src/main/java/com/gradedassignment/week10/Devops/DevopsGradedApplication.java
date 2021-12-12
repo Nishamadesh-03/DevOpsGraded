d1befcefd869c5391804af52723814e80f4ace6b
Nishamadesh03 <nisham@hcl.com>
2021-12-12 21:04:46 +0530

    first commit to devops graded

---
+++ src/main/java/com/gradedassignment/week10/Devops/DevopsGradedApplication.java
@@ -0,0 +1,13 @@
+package com.gradedassignment.week10.Devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGradedApplication.class, args);
+		System.out.println("Hello Dev-Ops");	}
+
+}
